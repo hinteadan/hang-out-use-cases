549f3ef7fe3a7ddffb2e5eb91a14e43b73cfab78
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-04 12:14:29 +0200

    OD: Scaffold web solution

---
+++ _references.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="app/bower_components/h.scoreKeeper/h.scoreKeeper.js" />
+/// <reference path="app/bower_components/lodash/dist/lodash.js" />
+/// <reference path="app/bower_components/jquery.gracket/jquery.gracket.js" />
+/// <reference path="app/scripts/tournamentWizard.js" />
+/// <reference path="app/bower_components/angular/angular.js" />
+/// <reference path="app/bower_components/angular-cookies/angular-cookies.js" />
+/// <reference path="app/bower_components/h.dataStore/h.dataStore.js" />
+
+/// <reference path="app/bower_components/jasmine/lib/jasmine-core/jasmine.js" />
+/// <reference path="app/bower_components/jasmine/lib/jasmine-core/boot/boot.js" />
+/// <reference path="app/scripts/common/localStore.js" />
+/// <reference path="app/bower_components/jsSHA/src/sha.js" />
+/// <reference path="app/bower_components/notifyjs/dist/notify.js" />
+/// <reference path="app/bower_components/moment/moment.js" />
+
+
+/// <reference path="app/scripts/users/user.js" />
+/// <reference path="app/scripts/users/module.js" />
+/// <reference path="app/scripts/users/hasher.js" />
