a00e7dbc5375b6a46c92acdef5c4fd34a5173974
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-04 16:37:21 +0200

    Scaffold join an activity

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,6 +10,7 @@
         $route
             .when('/', { templateUrl: 'scripts/use-cases.tmpl.html', controller: 'use-cases' })
             .when('/new', { templateUrl: 'scripts/new-activity.tmpl.html', controller: 'new-activity' })
+            .when('/join', { templateUrl: 'scripts/join-activity.tmpl.html', controller: 'join-activity' })
             .otherwise({ redirectTo: '/' });
     }]);
 
---
+++ app/scripts/join-activity-ctrl.js
@@ -0,0 +1,11 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('hang-out')
+    .controller('join-activity', [function () {
+
+
+
+    }]);
+
+}).call(this, this.angular);
