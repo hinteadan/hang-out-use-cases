57d79f71a3500b2e2609b73f9a430d782333f2f7
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-11-06 11:13:24 +0200

    Status view

--- app/scripts/status-ctrl.js
+++ app/scripts/status-ctrl.js
@@ -2,23 +2,28 @@
     'use strict';
 
     angular.module('hang-out')
-    .controller('status', ['$scope', 'model', 'dataStore', function ($s, m, store) {
+    .controller('status', ['$scope', '$q', 'model', 'dataStore', function ($s, $q, m, store) {
 
         var me = new m.Individual();
+
+        $s.flag = {
+            hasQueried: false,
+            isLoadingMyActivities: false,
+            isLoadingMyAppliedForActivities: false,
+        };
 
         $s.me = me;
         $s.myActivities = [];
         $s.myAppliedActivities = [];
 
         $s.statusForMe = function () {
-            store.activitiesFor(me)
-            .then(function (activities) {
-                $s.myActivities = activities;
-            });
-
-            store.activitiesAppliedToFor(me)
-            .then(function (activities) {
-                $s.myAppliedActivities = activities;
+            $q.all([store.activitiesFor(me), store.activitiesAppliedToFor(me)])
+            .then(function(activitiesPool){
+                $s.myActivities = activitiesPool[0];
+                $s.flag.isLoadingMyActivities = false;
+                $s.myAppliedActivities = activitiesPool[1];
+                $s.flag.isLoadingMyAppliedForActivities = false;
+                $s.flag.hasQueried = true;
             });
         };
 
